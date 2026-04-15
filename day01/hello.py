51ebbc3606944ce284e1bbc1f2421440b7d5faf4
lth0330 <dlxogud555@gmail.com>
2026-04-15 16:31:05 +0900

    python start

--- day01/hello.py
+++ day01/hello.py
@@ -1,1 +1,12 @@
 print(" hello cording python")
+
+# 인터프리터란? 
+# 파이썬 코드를 실행할 수 있는 도구( 한줄 단위 번역) python vs  컴파일 (파일 단위 번역 )
+
+# 표현식, 식별자, 키워드, 연산자  ===> 문장(코드) ===> 프로그램
+
+# 키워드
+import keyword
+print(keyword.kwlist)
+
+# 식별자 : 이름을 붙인 단어 , 변수명/함수명/클래스명 등등
