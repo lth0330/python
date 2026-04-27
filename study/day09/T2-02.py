acbcd68761a9a66ddbd9a7eeb38b1c81e8fcfbb4
lth0330 <dlxogud555@gmail.com>
2026-04-27 17:10:33 +0900

    04-27(numpy)

--- study/day09/T2-02.py
+++ study/day09/T2-02.py
@@ -23,3 +23,25 @@ print(x.ndim)  # 3
 x = np.array([[1,2],[3,4], [7,5]])
 for element in x.flat :
     print(element)
+
+# [3] 배열의 데이터 타입
+
+# .array(자료, dtype= numpy.타입명)
+x = np.array([1,2,3], dtype=np.int64)
+print(x.dtype)
+
+# 필요에 따라 적절하게 bit 선택한다.
+x = np.array([1.0,2.0,3.0], dtype=np.float64)
+print(x.dtype)
+
+x = np.array([True, False, True], dtype=np.bool_)
+print(x.dtype)  # bool , 논리형
+
+x = np.array(["apple","banana","cherry"], dtype=np.bytes_)
+print(x.dtype)   # 문자열을 바이트형태로 저장 
+
+# astype(numpy.변환할 타입명) , 타입변환
+x = np.array([1.5,3.3,4.7])  # 실수
+print(x.dtype)
+y = x.astype(np.int32)  # float64 -> int32
+print(y)   # [1 3 4] , int(정수형)은 소수점을 표현할 수 없으므로 소수점 잘림.
